1ce49cb12d8ad79e8f6a0225ed64772466b13b6c
martinbouhier <martinbouhier@outlook.com>
2024-05-19 13:06:44 -0300

    iterate page by page

--- src/app/movies/[id]/page.js
+++ src/app/movies/[id]/page.js
@@ -3,37 +3,49 @@ import { useState, useEffect } from "react";
 
 export default function PageDetails({ params }) {
   const { id } = params;
-  const [movie, setMovie] = useState([]);
+  const [movie, setMovie] = useState(null);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    fetch(
-      "https://mflixbackend.azurewebsites.net/api/movies?pageSize=10&page=1"
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setMovie(data.find((movie) => movie._id === id));
-      })
-      .catch((error) => console.log(error));
-  }, []);
+    if (!movie) {
+      fetch(
+        `https://mflixbackend.azurewebsites.net/api/movies?pageSize=10&page=${page}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          const foundMovie = data.find((movie) => movie._id === id);
+          if (foundMovie) {
+            setMovie(foundMovie);
+          } else {
+            setPage(page + 1);
+          }
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [page, movie, id]);
 
-return (
-  <div className="flex flex-col items-center justify-center min-h-screen py-2">
-    <div className="flex flex-col bg-white rounded-lg shadow-lg overflow-hidden max-w-md">
-      <div className="flex-shrink-0">
-        <img className="h-48 w-full object-cover" src={movie.poster} alt={movie.title} />
-      </div>
-      <div className="p-6">
-        <h2 className="text-2xl font-bold leading-7 text-black sm:text-3xl sm:leading-9 sm:truncate">
-          {movie.title}
-        </h2>
-        <div className="mt-2 text-base text-gray-500">
-          {movie.plot}
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <div className="flex flex-col bg-white rounded-lg shadow-lg overflow-hidden max-w-md">
+        <div className="flex-shrink-0">
+          <img className="h-48 w-full object-cover" src={movie.poster} alt={movie.title} />
         </div>
-        <div className="mt-2 text-base text-gray-500">
-          {movie.fullplot}
+        <div className="p-6">
+          <h2 className="text-2xl font-bold leading-7 text-black sm:text-3xl sm:leading-9 sm:truncate">
+            {movie.title}
+          </h2>
+          <div className="mt-2 text-base text-gray-500">
+            {movie.plot}
+          </div>
+          <div className="mt-2 text-base text-gray-500">
+            {movie.fullplot}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
 }
--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -4,21 +4,37 @@ import MovieList from "./MovieList.jsx";
 
 export default function MoviePage() {
   const [movies, setMovies] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     fetch(
-      "https://mflixbackend.azurewebsites.net/api/movies?pageSize=10&page=1"
+      `https://mflixbackend.azurewebsites.net/api/movies?pageSize=50&page=${currentPage}`
     )
       .then((response) => response.json())
       .then((data) => {
         setMovies(data);
       })
       .catch((error) => console.error(error));
-  }, []);
+  }, [currentPage]);
+
+  const nextPage = () => {
+    setCurrentPage(currentPage + 1);
+    window.scrollTo(0, 0);
+  };
+  
+  const prevPage = () => {
+    setCurrentPage(currentPage > 1 ? currentPage - 1 : 1);
+    window.scrollTo(0, 0);
+  };
 
   return (
     <div>
+      <h1 className="text-3xl font-bold text-center mt-4">Movies</h1>
       <MovieList movies={movies} />
+      <div className="flex justify-center mt-4">
+        <button onClick={prevPage} className="px-4 py-2 mr-2 bg-blue-500 text-white rounded m-20">Previous</button>
+        <button onClick={nextPage} className="px-4 py-2 ml-2 bg-blue-500 text-white rounded m-20">Next</button>
+      </div>
     </div>
   );
 }
